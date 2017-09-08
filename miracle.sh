278ec6a43459927375a9bf2af231d7a31ab516e8
4O4 <pk.pawelo@gmail.com>
2017-09-08 12:30:22 +0200

    Make debug info more visible

--- miracle.sh
+++ miracle.sh
@@ -156,7 +156,7 @@ install_with_sqlplus() {
 
 				show_errors_cmd="$(cat ${i} | grep -i -E 'create.*(package|package body|view|procedure).*[i|a]s' | perl -pe 's/create.*(package body|package|view|procedure).*?((["]?[a-z]{1,20}["]?\.)?["]?[a-zA-Z0-9_]{1,30}["]?).*[i|a]s/show errors \1 \2;/gi')"
  
-				echo ${show_errors_cmd}
+				printf "MIRACLE INFO: show_errors_cmd: ${show_errors_cmd}\n\n"
 
 				printf "${INSTALLATION_STARTED_FORMAT}" "Installing ${i}..."
 				result=$(sqlplus -s ${username}/${password} <<-EOF
